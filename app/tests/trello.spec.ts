f15f7c67bc9b98b92191aeae111d6f7b6a6f9bb0
Gokay Yilmaz <gok96@icloud.com>
2025-10-09 17:01:18 +0300

    before tutorial3-step3

---
+++ app/tests/trello.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeAll(async ({ request }) => {
+  // Clear the database
+  await request.post('http://localhost:3000/api/reset');
+});
+
+test('create board-list-card', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByPlaceholder('Name of your first board').fill('Chores');
+  await page.getByPlaceholder('Name of your first board').press('Enter');
+  
+  await expect(page.locator('[name="board-title"]')).toHaveValue('Chores');
+  await expect(page.getByPlaceholder('Enter list title...')).toBeVisible();
+  await expect(page.getByTestId('list')).not.toBeVisible();
+
+  await page.getByPlaceholder('Enter list title...').click();
+  await page.getByPlaceholder('Enter list title...').fill('TODO');
+  await page.getByPlaceholder('Enter list title...').press('Enter');
+  await page.getByText('Add another card').click();
+  await page.getByPlaceholder('Enter a title for this card...').fill('Buy groceries');
+  await page.getByRole('button', { name: 'Add card' }).click();
+  await page.getByPlaceholder('Enter a title for this card...').click();
+  await page.getByPlaceholder('Enter a title for this card...').fill('Mow the lawn');
+  await page.getByRole('button', { name: 'Add card' }).click();
+  await page.getByPlaceholder('Enter a title for this card...').click();
+  await page.getByPlaceholder('Enter a title for this card...').fill('Walk the dog');
+  await page.getByRole('button', { name: 'Add card' }).click();
+  await page.getByRole('navigation').getByRole('button').click();
+
+});
+
+// test('dummy', async ({ page }) => {});
